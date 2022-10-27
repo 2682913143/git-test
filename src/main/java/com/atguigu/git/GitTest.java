3ee6c727a731aef5ad8d71e3a65a8b955c665b67
jack <jack@qq.com>
2022-10-27 14:13:32 +0800

    master commit3

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -3,9 +3,9 @@ package com.atguigu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git1");
-        System.out.println("hello git22");
-        System.out.println("hello git333");
+        System.out.println("hello git2");
+        System.out.println("hello git3");
         System.out.println("hello git4444");
-        System.out.println("hello git55555");
+        System.out.println("hello git master");
     }
 }
